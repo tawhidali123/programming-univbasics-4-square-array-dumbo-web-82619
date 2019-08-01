8e6e943d381988558f789aad473c132e735e7b60
Tawhid Ali <tawhidali126@gmail.com>
2019-08-01 02:10:09 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  empty = []
+  
+  while counter < array.length do
+    item = array[counter]
+    convert = item **2
+    empty.push(convert)
+    counter += 1
+  end
+  return empty
 end
